6bec6d6fa24dd22996227bc0e1f078f786f8f917
mondzik <mondzik777@gmail.com>
2017-01-16 22:53:36 +0100

    Pierwszy szablon.

---
+++ src/main/java/MondziuTeam/App.java
@@ -0,0 +1,13 @@
+package MondziuTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
